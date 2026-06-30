4f12c6a9fd5bb07aaa1771e31860b7dfd1b12335
AvatarN03 <jsprashanth003@gmail.com>
2026-06-30 19:17:16 +0530

    feat: setup sentry on frontend and add necessary packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,50 @@
+import { Show, SignInButton, SignUpButton, useAuth, UserButton } from "@clerk/react";
+import { Routes, Route, Navigate } from "react-router";
+
+import PageLoader from "./components/PageLoader";
+import { Layout } from "./components/Layout";
 
 
 function App() {
 
+  const { isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return <PageLoader />;
+  }
 
   return (
     <>
-      <h1>Welcome to the App!, My Ramu</h1>
+      <Layout>
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/product/:slug" element={<ProductDetailPage />} />
+          <Route
+            path="/orders"
+            element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace />}
+          />
+          <Route path="/checkout/return" element={<CheckoutReturnPage />} />
+
+          <Route path="/demo-sentry" element={<SentryDemoPage />} />
+
+          <Route
+            path="/orders/:id/call"
+            element={isSignedIn ? <OrderVideoPage /> : <Navigate to={"/"} replace />}
+          />
+
+          <Route
+            path="/admin"
+            element={isSignedIn ? <AdminProductsPage /> : <Navigate to="/" replace />}
+          /> */}
+
+          {/* NESTED ROUTES */}
+          {/* <Route path="/orders/:id" element={<OrderDetailPage />}>
+            <Route index element={<OrderSummaryPage />} />
+            <Route path="chat" element={<OrderChatPage />} />
+          </Route> */}
+        </Routes>
+      </Layout>
     </>
   )
 }
